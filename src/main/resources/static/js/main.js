157e2ba7064aa53aa429efab6820ca830af197ad
Juunie Roh <blacklily7513@gmail.com>
2023-05-03 10:02:16 +0900

    건물 모델 업데이트 및 호버/클릭 기능 구현 완료

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -4,10 +4,9 @@ import { MapControls } from 'three/addons/controls/MapControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { FontLoader } from 'three/addons/loaders/FontLoader.js';
 
-let camera, controls, scene, renderer;
-const receivedBuildings = [];
-const buildings = [];
-const modals = [];
+// basic javascripts
+
+const receivedData = [];
 
 const fixedHelp = document.getElementById( 'fixedHelp' );
 fixedHelp.addEventListener( 'click', () => {
@@ -25,22 +24,43 @@ fixedHelp.addEventListener( 'click', () => {
 
 } );
 
+///////////////////////////////
+///// THREE.js from here: /////
+///////////////////////////////
+
+let width, height, camera, controls, scene, renderer, raycaster, gui;
+
+const pointer = new THREE.Vector2(); // mouse cursor position tracking
+let intersects = []; // list to find which building is selected
+let INTERSECTED = undefined; // stores which building is selected
+
+const buildings = [];
+const modals = [];
+const arrows = [];
+
 init();
-//render(); // remove when using next line for animation loop (requestAnimationFrame)
 animate();
 
 function init() {
+
+  // variables
+
+  width = window.innerWidth;
+  height = window.innerHeight - 1; // prevent generating scroll bar
 
   scene = new THREE.Scene();
   scene.background = new THREE.Color( 0xcccccc );
   scene.fog = new THREE.FogExp2( 0xcccccc, 0.002 );
 
+  raycaster = new THREE.Raycaster(); // for mouse(pointer) tracking
+
   renderer = new THREE.WebGLRenderer( { antialias: true } );
   renderer.setPixelRatio( window.devicePixelRatio );
-  renderer.setSize( window.innerWidth, window.innerHeight - 80 );
-  document.querySelector('main').appendChild( renderer.domElement );
+  renderer.setSize( width, height );
+  // where to append
+  document.querySelector( 'main' ).appendChild( renderer.domElement );
 
-  camera = new THREE.PerspectiveCamera (60, window.innerWidth / window.innerHeight, 1, 1000 );
+  camera = new THREE.PerspectiveCamera( 60, width / height, 1, 1000 );
   camera.position.set( 400, 200, 0 );
 
   // controls
@@ -55,16 +75,18 @@ function init() {
   controls.screenSpacePanning = false;
 
   controls.minDistance = 100;
-  controls.maxDistance = 500;
+  controls.maxDistance = 700;
 
   controls.maxPolarAngle = Math.PI / 2;
 
   // GLTF Loader
+
   const gltfLoader = new GLTFLoader();
-  loadModel( gltfLoader, './models/SaeBit.glb', new THREE.Vector3( 112, 0, -460 ), 'SaeBit', -106, 2 );
-  loadModel( gltfLoader, './models/HwaDo.glb', new THREE.Vector3(-30, 0, -210), 'HwaDo', -118, 2);
+  createModel( gltfLoader, './models/SaeBit.glb', new THREE.Vector3( 112, 0, -460 ), 'SaeBit', -106, 2 );
+  createModel( gltfLoader, './models/HwaDo.glb', new THREE.Vector3( -30, 0, -210 ), 'HwaDo', -118, 2 );
 
   // world floor
+
   const planeSize = 2000;
   const planeTexture = new THREE.TextureLoader().load( './images/KakaoMap_KWU.png' );
   const worldFloor = new THREE.Mesh(
@@ -73,56 +95,87 @@ function init() {
   );
   worldFloor.rotateX( Math.PI / (-2) );
   worldFloor.rotateZ( Math.PI / 2 );
+  worldFloor.name = 'worldFloor';
   scene.add(worldFloor);
 
   // lights
 
   const dirLight1 = new THREE.DirectionalLight( 0xffffff );
   dirLight1.position.set( 10, 12, 9 );
+  dirLight1.name = 'dirLight1';
   scene.add( dirLight1 );
 
   const dirLight2 = new THREE.DirectionalLight( 0x002266 );
   dirLight2.position.set( -9, -12, -10 );
+  dirLight2.name = 'dirLight2';
   scene.add( dirLight2 );
 
   const ambientLight = new THREE.AmbientLight( 0x222222 );
+  ambientLight.name = 'ambientLight';
   scene.add( ambientLight );
 
-  //
+  // Create GUI Pannel
 
-  window.addEventListener( 'resize', onWindowResize );
-
-  // Create GUI Control Pannel
-  const gui = new GUI( { container: document.getElementById( 'guiContainer' ), title: 'Information' } );
+  gui = new GUI( { container: document.getElementById( 'guiContainer' ), title: 'Information' } );
   let obj = {
     myBoolean: false,
-    myString: 'Test String',
+    Name: 'stringName',
     myNumber: 512,
     myFunction: function() { alert( 'hi' ) }, // onclick callback
-    onClickSaeBit: function() { gui.controllers[ 1 ].setValue( '새빛관 테스트 버튼 clicked' ); gui.controllers[ 2 ].setValue( 9 ); }, // onclick saebit here (test button)
-    onClickHwaDo: function() { gui.controllers[ 1 ].setValue( '화도관 테스트 버튼 clicked' ); gui.controllers[ 2 ].setValue( 6 ); }, // onclick hwado here (test button)
   }
   
   gui.add( obj, 'myBoolean' ); 	// checkbox
-  gui.add( obj, 'myString' ); 	// text field
+  gui.add( obj, 'Name' ); 	// text field
   gui.add( obj, 'myNumber' ); 	// number field
-  gui.add( obj, 'myFunction' ).name( '테스트 링크' ); 	// button
-  gui.add( obj, 'onClickSaeBit' ).name( '새빛관 onClick test' );
-  gui.add( obj, 'onClickHwaDo' ).name( '화도관 onClick test' );
+  gui.add( obj, 'myFunction' ).name( 'alert hi' ); 	// button
+
+  window.addEventListener( 'resize', onWindowResize );
+  window.addEventListener( 'pointermove', onPointerMove );
+  window.addEventListener( 'click', onClick );
+  window.addEventListener( 'dblclick', ( event ) => { // dev
+    // 더블 클릭시 카메라의 위치에서 카메라 방향으로 
+    console.log( event );
+    const arrow = new THREE.ArrowHelper( camera.getWorldDirection( new THREE.Vector3 ), camera.getWorldPosition( new THREE.Vector3 ), 100, 0xff0000 );
+    scene.add( arrow );
+    arrows.push( arrow );
+  } );
+
 }
+
+// window events
 
 function onWindowResize() {
-
-  camera.aspect = window.innerWidth / window.innerHeight;
+  
+  camera.aspect = width / height;
   camera.updateProjectionMatrix();
-  renderer.setSize( window.innerWidth, window.innerHeight );
+  renderer.setSize( width, height );
+  
+}
+
+function onPointerMove( event ) {
+
+  pointer.set( ( event.clientX / width ) * 2 - 1, - ( event.clientY / height ) * 2 + 1 );
+  getIntersects();
 
 }
+
+function onClick( event ) {
+
+  if ( INTERSECTED ) {
+
+    INTERSECTED.userData.onClick();
+
+  }
+
+}
+
+// three.js required
 
 function animate() {
 
-  requestAnimationFrame( animate );
-  // Let the groups generated from `createMedal()` to face camera all the time
+  window.requestAnimationFrame( animate );
+
+  // Let the groups generated from `createFont()` to face camera all the time
   modals.forEach( ( modal ) => {
 
     modal.quaternion.copy( camera.quaternion );
@@ -139,36 +192,68 @@ function render() {
 
 }
 
+// customed functions
+
 /**
- * 건물 모델, `*.glb`를 불러옵니다.
+ * 건물의 모델링을 불러와 `scene`에 추가합니다.
  * 
  * `loader` 를 사용해 `modelPath` 에 있는 모델을 불러옵니다.   
  * 모델의 위치, 회전시킬 각도, 크기 조정을 위한 스케일을 설정하여 `scene` 및 `buildings` 리스트에 추가하고, `createModal()` 에 `position` 을 전달합니다.
- * 
- * 모델만 불러오고, 모델의 position, angle, scale 을 설정하는 기능을 나눠야함.
  * @param { GLTFLoader } loader `GLTFLoader` used in this file.
- * @param { string } modelPath 
- * @param { THREE.Vector3 } position 
- * @param { string } name 
- * @param { number } angle 
- * @param { number } scale 
+ * @param { string } modelPath string path url of target `*.glb` file.
+ * @param { THREE.Vector3 } position a position where this model will be placed.
+ * @param { string } name name of this model.
+ * @param { number } angle rotation angle applied to `rotateY`.
+ * @param { number } scale recommended value is 2.
  */
-async function loadModel ( loader, modelPath, position, name, angle, scale ) {
+function createModel ( loader, modelPath, position, name = '', angle = 0, scale = 2 ) {
 
-  await loader.load( modelPath, async ( gltf ) => {
+  if ( modelPath === '' ) { console.error( 'modelPath not found' ); return; }
+  loader.load( modelPath, async ( gltf ) => {
 
     const model = await gltf.scene;
+    model.name = name;
     model.position.copy( position );
     model.rotateY( Math.PI / 180 * angle );
     model.scale.setScalar( scale );
+
+    // add events to this model via userData
+    model.userData = {
+      isActive: false,
+
+      onPointerOver: function() {
+        for ( let child of model.children ) {
+
+          child.currentHex = child.material.emissive.getHex();
+          child.material.emissive.setHex( 0xff0000 );
     
-    createModal( model.position, name );
+        }
+      },
+
+      onPointerOut: function() {
+        for ( let child of model.children ) {
+
+          child.material.emissive.setHex( 0 );
+
+        }
+      },
+
+      onClick: function() {
+
+        console.log( model.name + ' clicked!' );
+        gui.open();
+        gui.controllers[ 1 ].setValue( model.name );
+
+      }
+    }
+    
+    createFont( model.position, name );
     buildings.push( model );
     scene.add( model );
 
   }, ( progress ) => {
 
-    console.log( progress.loaded / progress.total * 100 + "% loaded!" );
+    // console.log( progress.loaded / progress.total * 100 + "% loaded!" );
 
   }, ( error ) => {
 
@@ -183,8 +268,9 @@ async function loadModel ( loader, modelPath, position, name, angle, scale ) {
  * `position` 에 해당하는 위치에서 `THREE.Line` 과 `THREE.Mesh (text)` 을 갖는 `THREE.Group` 을 생성합니다.
  * @param { THREE.Vector3 } position position of the target model
  * @param { string } name name of the target building
+ * @notice 현재 사용하는 폰트는 한글이 지원되지 않습니다. `name` 의 값이 한글일 경우, 물음표로 표시됩니다.
  */
-function createModal( position, name ) {
+function createFont( position, name ) {
   // Drawing Lines:
   const points = [];
   points.push( new THREE.Vector3( 0, 0, 0 ) );
@@ -197,39 +283,27 @@ function createModal( position, name ) {
   line.position.set( 0, 0, 0 );
   line.material.depthTest = false; // for renderOrder
 
-  // Create Plane:
-  // (Will be replaced by Text)
-  // const plane = new THREE.Mesh(
-  //   new THREE.PlaneGeometry( 50, 50, 8, 8, ),
-  //   new THREE.MeshBasicMaterial( { color: 0x000000, side: THREE.FrontSide, transparent: true, opacity: 0.5 } )
-  // );
-  // plane.position.set( 50, 50, 50 );
-  // plane.material.depthTest = false; // for renderOrder
-
+  // font loading function
   const loader = new FontLoader();
-  loader.load('./fonts/helvetiker_bold.typeface.json', function (font) {
+  loader.load( './fonts/helvetiker_bold.typeface.json', function ( font ) {
 
-    const material = new THREE.MeshBasicMaterial({
+    const material = new THREE.MeshBasicMaterial( {
       color: 0x000000,
       transparent: false,
       side: THREE.FrontSide,
-    });
+    } );
 
     const message = name;
-    const shapes = font.generateShapes(message, 10);
-    const geometry = new THREE.ShapeGeometry(shapes);
-
-    geometry.computeBoundingBox();
-    const yMid = 0.5 * (geometry.boundingBox.max.y - geometry.boundingBox.min.y);
-    // geometry.translate(0, yMid, 0);
+    const shapes = font.generateShapes( message, 10 );
+    const geometry = new THREE.ShapeGeometry( shapes );
 
     // make shape ( N.B. edge view not visible )
-    const text = new THREE.Mesh(geometry, material);
+    const text = new THREE.Mesh( geometry, material );
     text.position.set( 50, 50, 50 );
     text.material.depthTest = false; // for renderOrder
-    group.add(text);
+    group.add( text );
 
-  }); //end load function
+  } ); //end load function
 
   // Create Group:
   const group = new THREE.Group();
@@ -238,6 +312,32 @@ function createModal( position, name ) {
   group.position.copy( position );
   modals.push( group );
   group.renderOrder = 1; // renderOrder (z-index)
+  group.name = name + ' Modal';
   scene.add( group );
+
+}
+
+/**
+ * `pointer` 에서 `camera` 가 바라보는 방향으로 `raycaster` 를 생성해 교차하는 아이템을 가져옵니다.   
+ * 
+ * `buildings` 목록에서 `raycaster` 와 교차하는 아이템을 확인하여 가장 앞에 있는 것을 `INTERSECTED`로 설정한 후 `onPointerOver()` 를 수행합니다.   
+ * 교차하는 아이템이 바뀌거나 사라졌을 때는 기존 아이템의 `onPointerOut()`를 수행합니다.
+ */
+function getIntersects() {
+
+  raycaster.setFromCamera( pointer, camera );
+  intersects = raycaster.intersectObjects( buildings, true );
+  if ( intersects.length > 0 ) { 
+    
+    if ( INTERSECTED ) INTERSECTED.userData.onPointerOut();
+    INTERSECTED = intersects[ 0 ].object.parent;
+    INTERSECTED.userData.onPointerOver();
+  
+  } else { 
+    
+    if ( INTERSECTED ) INTERSECTED.userData.onPointerOut();
+    INTERSECTED = undefined;
+  
+  }
 
 }
