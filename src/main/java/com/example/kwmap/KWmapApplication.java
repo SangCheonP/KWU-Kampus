010d6a5fa9c5ea4f483f869c60cd35d09f524cdd
Park SangCheon <84065815+SangCheonP@users.noreply.github.com>
2023-06-02 22:23:26 +0900

    06/02 .war 설정
     - war 배포 설정

--- src/main/java/com/example/kwmap/KWmapApplication.java
+++ src/main/java/com/example/kwmap/KWmapApplication.java
@@ -2,12 +2,27 @@ package com.example.kwmap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class KWmapApplication {
+public class KWmapApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(KWmapApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(KWmapApplication.class, args);
     }
 
 }
+
+//public class KWmapApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(KWmapApplication.class, args);
+//    }
+//
+//}
