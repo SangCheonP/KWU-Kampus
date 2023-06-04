772e23d9483aba7009efdc57a6cc33dc4337395c
Park SangCheon <84065815+SangCheonP@users.noreply.github.com>
2023-06-04 20:05:51 +0900

    06/04 외부 tomcat으로 설정

--- src/main/java/com/example/kwmap/KWmapApplication.java
+++ src/main/java/com/example/kwmap/KWmapApplication.java
@@ -6,23 +6,23 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-//public class KWmapApplication extends SpringBootServletInitializer {
-//
-//    @Override
-//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-//        return application.sources(KWmapApplication.class);
-//    }
-//
-//    public static void main(String[] args) {
-//        SpringApplication.run(KWmapApplication.class, args);
-//    }
-//
-//}
+public class KWmapApplication extends SpringBootServletInitializer {
 
-public class KWmapApplication {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(KWmapApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(KWmapApplication.class, args);
     }
 
 }
+
+//public class KWmapApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(KWmapApplication.class, args);
+//    }
+//
+//}
