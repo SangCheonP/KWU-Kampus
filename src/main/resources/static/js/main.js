c7eb78ec530214e6c6559bd313354ac4c8246030
yumi.shim <yeecon@naver.com>
2023-06-13 18:15:21 +0900

    06/13 메인 화면 수정

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -151,7 +151,6 @@ function init() {
 
   setInfo(info_help, help_content);
   infoButton[0].textContent = '만족도 조사 하기';
-  // document.getElementById( 'infoHref' ).href = 'https://forms.gle/UuJ6kk4r8Gxd6ZT99';
 
   infoPage.classList.remove('on');
   infoTag.classList.remove('on');
