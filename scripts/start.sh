99ba6761a61ea5a7b4ec572e12393f842dbd630f
Park SangCheon <84065815+SangCheonP@users.noreply.github.com>
2023-07-06 23:35:53 +0900

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -10,7 +10,7 @@ START_LOG="$ROOT_PATH/start.log"
 NOW=$(date +%c)
 
 echo "[$NOW] $WAR 복사" >> $START_LOG
-cp $ROOT_PATH/build/libs/spring-github-action-1.0.0.war $WAR
+cp $ROOT_PATH/build/libs/kampus.war $WAR
 
 echo "[$NOW] > $WAR 실행" >> $START_LOG
 nohup java -war $WAR > $APP_LOG 2> $ERROR_LOG &
