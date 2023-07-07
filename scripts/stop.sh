1beb420f742b673b18fe5786d5b46c0ef9d806ba
Park SangCheon <84065815+SangCheonP@users.noreply.github.com>
2023-07-07 09:31:20 +0900

    Update stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 ROOT_PATH="/home/ubuntu/spring-github-action"
-JAR="$ROOT_PATH/KWU Campus-0.0.1-SNAPSHOT.jar"
+JAR="$ROOT_PATH/'KWU Campus-0.0.1-SNAPSHOT.jar'"
 STOP_LOG="$ROOT_PATH/stop.log"
 
 # 실행중인 Spring 서버의 PID
@@ -11,6 +11,6 @@ if [ -z "$SERVICE_PID" ]; then
   echo "서비스 NotFound" >> $STOP_LOG
 else
   echo "서비스 종료 " >> $STOP_LOG
-  kill -15 $SERVICE_PID
+  kill "$SERVICE_PID"
   # kill -9 $SERVICE_PID # 강제 종료를 하고 싶다면 이 명령어 사용
 fi
