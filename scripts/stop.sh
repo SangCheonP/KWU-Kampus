96d5587935d3704939f08f35503e352c410e4b3f
Park SangCheon <84065815+SangCheonP@users.noreply.github.com>
2023-07-07 09:43:54 +0900

    Update stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -3,14 +3,15 @@
 ROOT_PATH="/home/ubuntu/spring-github-action"
 JAR="$ROOT_PATH/kampus.jar"
 STOP_LOG="$ROOT_PATH/stop.log"
+NOW=$(date +%c)
 
 # 실행중인 Spring 서버의 PID
 SERVICE_PID=$(pgrep -f $JAR)
 
 if [ -z "$SERVICE_PID" ]; then
-  echo "서비스 NotFound" >> $STOP_LOG
+  echo "[$NOW] 서비스 NotFound" >> $STOP_LOG
 else
-  echo "서비스 종료 " >> $STOP_LOG
+  echo "[$NOW] 서비스 종료 " >> $STOP_LOG
   kill "$SERVICE_PID"
   # kill -9 $SERVICE_PID # 강제 종료를 하고 싶다면 이 명령어 사용
 fi
