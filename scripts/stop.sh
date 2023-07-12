ed0d4215bb94efb857dc043cb2fd38acffb534b9
Park SangCheon <84065815+SangCheonP@users.noreply.github.com>
2023-07-12 22:11:29 +0900

    Update stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -11,7 +11,7 @@ SERVICE_PID=$(pgrep -f java)
 if [ -z "$SERVICE_PID" ]; then
   echo "[$NOW] 서비스 NotFound" >> $STOP_LOG
 else
-  echo "[$NOW] 서비스 종료 " >> $STOP_LOG
+  echo "[$NOW] $SERVICE_PID 종료 " >> $STOP_LOG
   kill "$SERVICE_PID"
   # kill -9 $SERVICE_PID # 강제 종료를 하고 싶다면 이 명령어 사용
 fi
