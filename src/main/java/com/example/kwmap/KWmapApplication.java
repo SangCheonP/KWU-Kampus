6132a481b14732360cf8101422c463b2257baac8
uhcho2020 <dudgh1215@naver.com>
2023-09-24 20:51:29 +0900

    09/24 파이썬 파일 경로 수정(절대경로 -> 상대경로)

--- src/main/java/com/example/kwmap/KWmapApplication.java
+++ src/main/java/com/example/kwmap/KWmapApplication.java
@@ -52,7 +52,7 @@ class PythonScriptRunner implements CommandLineRunner {
     @Scheduled(cron = "0 0 0 * * ?")    // 매일 0시 정각에 실행
     private void executePythonScript() {
         try {
-            String pythonScriptPath = "src/main/python/dbconn_Test.py";
+            String pythonScriptPath = "src/main/python/dbconn_Test.py"; //상대경로로 파일 경로 수정
             /* inheritIO : JVM 프로세스 스트림으로 상속 */
             ProcessBuilder processBuilder = new ProcessBuilder("python3", pythonScriptPath);
             processBuilder.redirectErrorStream(true);
