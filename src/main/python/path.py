919c5f1efeaabb20b84f07c49d739525f2c51fc9
uhcho2020 <dudgh1215@naver.com>
2023-10-16 20:24:27 +0900

    10/16 환경변수 설정

--- src/main/python/path.py
+++ src/main/python/path.py
@@ -1,4 +1,8 @@
 import os
+import sys
+
+sys.path.append('/home/ubuntu/.local/lib/python3.9/site-packages')
+
 pythonpath = os.environ.get("PYTHONPATH")
 print(pythonpath)
 print("이후는 전체 환경 변수 및 해당 값")
