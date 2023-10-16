5406315f70647c57e7e272887908cb637fa7bcd8
uhcho2020 <dudgh1215@naver.com>
2023-10-16 15:23:41 +0900

    10/16 pythonScriptPath 수정

--- src/main/java/com/example/kwmap/KWmapApplication.java
+++ src/main/java/com/example/kwmap/KWmapApplication.java
@@ -49,10 +49,10 @@ class PythonScriptRunner implements CommandLineRunner {
         executePythonScript();
     }
 
-    @Scheduled(cron = "0 */10 * * * *")    // 매일 0시 정각에 실행
+    @Scheduled(cron = "0 */10 * * * *")
     private void executePythonScript() {
         try {
-            String pythonScriptPath = System.getProperty("user.dir") + "/src/main/python/dbconn_Test.py";
+            String pythonScriptPath = "/home/ubuntu/spring-github-action/src/main/python/dbconn_Test.py";
             System.out.println(System.getProperty("user.dir"));
             /* inheritIO : JVM 프로세스 스트림으로 상속 */
             ProcessBuilder processBuilder = new ProcessBuilder("python3", pythonScriptPath);
