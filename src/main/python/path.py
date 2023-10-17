be338fdcb1856c2932bd53b0dd72efdfa7b37390
uhcho2020 <dudgh1215@naver.com>
2023-10-17 11:23:26 +0900

    10/17 python 경로 탐색

--- src/main/python/path.py
+++ src/main/python/path.py
@@ -1,9 +1,9 @@
 import os
 import sys
 
-sys.path.append('/home/ubuntu/.local/lib/python3.9/site-packages')
-
-pythonpath = os.environ.get("PYTHONPATH")
-print(pythonpath)
-print("이후는 전체 환경 변수 및 해당 값")
-print(os.environ)
+# sys.path.append('/home/ubuntu/.local/lib/python3.9/site-packages')
+print(sys.executable)
+# pythonpath = os.environ.get("PYTHONPATH")
+# print(pythonpath)
+# print("이후는 전체 환경 변수 및 해당 값")
+# print(os.environ)
