3aa9dc6fa142b2e8b6cf0d8765229dbb3a6f4f1b
uhcho2020 <dudgh1215@naver.com>
2023-10-17 13:40:12 +0900

    10/17 python 크롤링 업데이트

--- src/main/python/dbconn_Test.py
+++ src/main/python/dbconn_Test.py
@@ -445,8 +445,8 @@ def update():
 
         Electronic_Information()
         Humanities_and_Social_Sciences()
-        Business()
         Software_Convergence()
+        Business()
         Ingenium()
         Engineering()
         Natural()
