30dd69a09556e1d136ca820cab50f06579c49589
uhcho2020 <dudgh1215@naver.com>
2023-10-22 21:37:15 +0900

    10/22 크롤링 자동화 test

--- src/main/java/com/example/kwmap/KWmapApplication.java
+++ src/main/java/com/example/kwmap/KWmapApplication.java
@@ -50,7 +50,7 @@ class PythonScriptRunner implements CommandLineRunner {
         executePythonScript();
     }
 
-    @Scheduled(cron = "0 38 21 * * ?")
+    @Scheduled(cron = "0 39 21 * * ?")
     private void executePythonScript() {
         try {
             String pythonScriptPath = "/home/ubuntu/spring-github-action/src/main/python/dbconn_Test.py";
